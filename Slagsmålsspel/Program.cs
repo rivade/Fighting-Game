9b2d493edccd6305fe7e3a1b8507194bf62d9d3d
theo lindqvist <theo.lindqvist@elev.ga.ntig.se>
2022-10-06 15:49:46 +0200

    Updated fighting system

--- Slagsmålsspel/Program.cs
+++ Slagsmålsspel/Program.cs
@@ -18,6 +18,7 @@ static int Fight(int health, int opphealth, int firststrike, string name, string
 {
     Random hit = new Random();
     int damage = 0;
+    int miss = 0;
     if (firststrike == 1)
     {
         int slap = hit.Next(5, 16);
@@ -41,7 +42,7 @@ static int Fight(int health, int opphealth, int firststrike, string name, string
         Console.WriteLine();
         Console.WriteLine("Du går till attack!");
         Console.WriteLine("Välj alternativ nedan.");
-        Console.WriteLine("[A] Slag (20-30 damage)   [B] Spark (10-40 damage)  [C] Knä (1-50 damage)");
+        Console.WriteLine("[A] Slag (20-30 damage, 80% träffchans)   [B] Spark (30-50 damage, 50% träffchans)  [C] Knä (40-60 damage, 25% träffchans)");
         string attack = Console.ReadLine().ToLower();
         while (attack != "a" && attack != "b" && attack != "c"){
             Console.WriteLine("Du måste välja antingen a, b eller c.");
@@ -51,14 +52,31 @@ static int Fight(int health, int opphealth, int firststrike, string name, string
             case "a":
                 damage = hit.Next(20, 31);
                 Console.WriteLine("Du slår din motståndare!");
+                miss = hit.Next(1, 6);
+                if (miss == 1){
+                Console.WriteLine("Du missade!");
+                damage = 0;
+                }
             break;
             case "b":
-                damage = hit.Next(10, 41);
+                damage = hit.Next(30, 51);
                 Console.WriteLine("Du sparkar din motståndare!");
+                miss = hit.Next(1, 3);
+                if (miss == 1){
+                Console.WriteLine("Du missade!");
+                damage = 0;
+                }
             break;
             case "c":
-                damage = hit.Next(1, 51);
                 Console.WriteLine("Du knäar din motståndare!");
+                miss = hit.Next(1, 5);
+                if (miss == 1){
+                damage = hit.Next(40, 61);
+                }
+                else{
+                    Console.WriteLine("Du missade!");
+                    damage = 0;
+                }
             break;
         }    
         int crit = hit.Next(1, 21);
@@ -67,11 +85,6 @@ static int Fight(int health, int opphealth, int firststrike, string name, string
             Console.WriteLine("Du knockade din motståndare!");
             opphealth = 0;
             break;
-        }
-        int miss = hit.Next(1, 6);
-        if (miss == 1){
-            Console.WriteLine("Du missade!");
-            damage = 0;
         }
         opphealth -= damage;
         Console.WriteLine($"Din attack gjorde {damage} skada!");
@@ -82,34 +95,50 @@ static int Fight(int health, int opphealth, int firststrike, string name, string
         else{
             break;
         }
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(char.ToUpper(name[0]) + name.Substring(1) + $": {health} HP          {oppName}: {opphealth} HP");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine();
         int oppattack = hit.Next(1, 4);
         switch(oppattack){
             case 1:
                 damage = hit.Next(20, 31);
-                Console.WriteLine("Din motståndare slår dig!");
+                Console.WriteLine($"{oppName} slår dig!");
+                miss = hit.Next(1, 6);
+                if (miss == 1){
+                Console.WriteLine($"{oppName} missade!");
+                damage = 0;
+                }
             break;
             case 2:
-                damage = hit.Next(10, 41);
-                Console.WriteLine("Din motståndare sparkar dig!");
+                damage = hit.Next(30, 51);
+                Console.WriteLine($"{oppName} sparkar dig!");
+                miss = hit.Next(1, 3);
+                if (miss == 1){
+                Console.WriteLine($"{oppName} missade!");
+                damage = 0;
+                }
             break;
             case 3:
-                damage = hit.Next(1, 51);
-                Console.WriteLine("Din motståndare knäar dig!");
+                Console.WriteLine($"{oppName} knäar dig!");
+                miss = hit.Next(1, 5);
+                if (miss == 1){
+                damage = hit.Next(40, 61);
+                }
+                else{
+                    Console.WriteLine($"{oppName} missade!");
+                    damage = 0;
+                }
             break;
         }
         crit = hit.Next(1, 21);
         if (crit == 5){
             Console.WriteLine("Kritisk träff!");
-            Console.WriteLine("Din motståndare knockar dig!");
+            Console.WriteLine($"{oppName} knockar dig!");
             health = 0;
             break;
         }
-        miss = hit.Next(1, 6);
-        if (miss == 5){
-            Console.WriteLine("Din motståndare missade!");
-            damage = 0;
-        }
-        
         Console.WriteLine($"Det gjorde {damage} skada!");
         health -= damage;
         Console.WriteLine("Tryck enter för att fortsätta");
@@ -130,7 +159,7 @@ static int Fight(int health, int opphealth, int firststrike, string name, string
     }
 }
 
-Console.WriteLine("Välkommen till malmö vev simulator!");
+Console.WriteLine("Välkommen till boxnings simulatorn!");
 Console.WriteLine("Är du redo att börja? [y/n] ");
 string begin = Console.ReadLine().ToLower();
 while (begin != "y" && begin != "n")
@@ -161,10 +190,9 @@ while (active == 1)
     }
     int num = rnd.Next(1, 4);
     string oppName = OpponentName(num);
-
     Console.Clear();
-    Console.WriteLine("Du är ute och går på promenad, när en tjej kommer fram till dig.");
-    Console.WriteLine($"{oppName}: Ey har do cigg?");
+    Console.WriteLine("Du är ute och går på promenad, när någon kommer fram till dig.");
+    Console.WriteLine($"{oppName}: Har do cigg?");
     Console.WriteLine("Vad svarar du? [y/n] ");
     begin = Console.ReadLine().ToLower();
     while (begin != "y" && begin != "n")
@@ -176,8 +204,8 @@ while (active == 1)
     if (begin == "n")
     {
         Console.WriteLine(char.ToUpper(name[0]) + name.Substring(1) + ": Nej tyvärr, jag har inte det är jag rädd");
-        Console.WriteLine($"{oppName}: EY DU JAG KNOLLAR DAJ");
-        Console.WriteLine($"{oppName} bitchslapar dig");
+        Console.WriteLine($"{oppName}: EY DU JAG KNOLLAR DIG");
+        Console.WriteLine($"{oppName} bitchslapar dig!");
         Console.WriteLine();
         int winCheck = Fight(100, 100, 1, name, oppName);
         Console.Clear();
